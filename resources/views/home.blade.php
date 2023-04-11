80a1f7b9338b0afc8d352b1afd3bb9b6d9c2fe5d
rini <201220054@unmuhpnk.ac.id>
2023-04-11 23:22:12 +0700

    rini

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('container')
     <h1>Halaman Utama</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod repudiandae aspernatur sed tempore quae id rerum molestiae possimus. Atque magnam facere eius alias vero eos esse deleniti explicabo aperiam possimus molestias reiciendis fuga voluptas similique placeat provident, quidem quo. Velit, facere alias dolorum officia ipsam aut non quaerat soluta obcaecati illo incidunt inventore iure, mollitia vel, exercitationem quos voluptatibus nulla placeat quibusdam atque! Dignissimos numquam saepe aut incidunt odio quia pariatur dolores facere sapiente recusandae ex nulla, sint architecto exercitationem eius. In ipsa aliquid, incidunt aspernatur sapiente, totam ratione repellat rerum explicabo aperiam officiis, laboriosam sint laborum iure vel eaque!</p>
 @endsection
