78e83a42802312157c099f769cddf72f7f3a55a7
Ella <201220081@unmuhpnk.ac.id>
2023-07-26 23:43:49 +0700

    kedua

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,18 @@
 
 @section('container')
     <h1>Halaman Contact</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt alias impedit doloremque aspernatur tempore
+        debitis porro vero consectetur ea optio inventore corrupti, adipisci nihil exercitationem enim. Voluptate velit
+        doloremque laboriosam tempore quae! Odit adipisci quae saepe voluptate eveniet optio placeat numquam similique
+        consectetur quod. Animi voluptas dignissimos, reprehenderit dolor beatae unde ipsa? Laboriosam aperiam magnam dolore
+        accusamus ut saepe eos consectetur perferendis doloremque dicta nulla nobis voluptas in nihil vel laborum tenetur
+        consequatur quasi esse, non earum, vero nam reprehenderit. Fuga dignissimos nobis, commodi, veritatis sint placeat
+        nostrum nihil porro, omnis ducimus necessitatibus corporis debitis tempora voluptatum autem ipsum. Repellat libero
+        beatae obcaecati quisquam inventore quas unde, eos provident, neque, tempore vitae illo eligendi quam fugiat
+        consequatur sit voluptatem iure iste dolores rem! Exercitationem voluptas cum necessitatibus sapiente, ut beatae
+        dicta architecto atque corporis eaque eos, itaque ad nulla deserunt reiciendis? Placeat velit dolor amet sit, ipsa
+        quisquam id laboriosam deleniti, delectus nihil temporibus iusto non. Dignissimos dolores ratione reiciendis magnam
+        explicabo dolorum quaerat pariatur, libero neque aliquid minima expedita illum similique! Quo tempore vero
+        exercitationem rerum deleniti, eum sapiente. Sint, doloremque. Temporibus iusto nulla autem excepturi rerum vel
+        quaerat eos, quisquam unde cumque. Ea eos id ullam nulla reprehenderit.</p>
 @endsection
